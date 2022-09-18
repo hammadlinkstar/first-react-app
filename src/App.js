eafdbaaab7c1b128f9e5fcef1c979b93794af575
Muhammad Hammad <hammadlinkstar>
2022-09-19 00:49:43 +0500

    update_app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
 
-      <Navbar title="Navbar"/>
+      <Navbar title="Navbar" />
       <div className='container my-3'>
 
-      <TextForm heading="Enter the text to analyze"/>
+        {/* <TextForm heading="Enter the text to analyze" /> */}
+        <About />
 
       </div>
 
